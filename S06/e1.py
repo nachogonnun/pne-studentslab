7a5ec5e29717faf47a9ae4277859cdaa0f0dfce4
nachogonnun <i.gonzalezn.2023@alumnos.urjc.es>
2024-02-13 18:05:26 +0100

    Revert "session 2 exercises"
    This reverts commit 3d58f42e5df0e9f3abad4fd3766fa37e4e2c9bf6.

--- S06/e1.py
+++ S06/e1.py
@@ -3,10 +3,7 @@ class Seq:
 
     def __init__(self, strbases):
         self.strbases = strbases
-        if self.check_base() == "ERROR!!":
-            print("ERROR!!")
-        else:
-            print("New sequence created!")
+        print("New sequence created!")
     def __str__(self):
         return self.strbases
 
@@ -15,12 +12,11 @@ class Seq:
         for i in self.strbases:
             if i not in bases:
                 return "ERROR!!"
-        return self.strbases
+        return "New sequence created!!"
 
 
-##MAIN PROGRAM##
+
 s1 = Seq("ACCTGC")
 s2 = Seq("Hello? Am I a valid sequence?")
-print(f"Sequence 1: {s1.check_base()}")
-print(f"Sequence 2: {s2.check_base()}")
-
+print(f"Sequence 1: {s1}")
+print(f"Sequence 2: {s2}")
--- S06/test-color.py
+++ S06/test-color.py
@@ -1,5 +1,3 @@
 import termcolor
 
 termcolor.cprint("Hey! this is printed in green!", 'yellow')
-
-
