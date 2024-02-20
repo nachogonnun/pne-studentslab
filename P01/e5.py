559028db56a728520022476f70fbd8db93e6ba06
nachogonnun <i.gonzalezn.2023@alumnos.urjc.es>
2024-02-20 23:30:16 +0100

    .

--- P01/e5.py
+++ P01/e5.py
@@ -1,38 +1,7 @@
-class Seq:
-
-    def __init__(self, sequence = None):
-        self.sequence = sequence
-        if self.check_base() == "ERROR!!":
-            print("INVALID SEQUENCE!")
-        elif self.sequence == "NULL":
-            print("Null sequence created")
-        else:
-            print("New sequence created!")
-
-    def __str__(self):
-        """Method called when the object is being printed"""
-        # -- We just return the string with the sequence
-        return self.sequence
-
-    def check_base(self):
-        bases = ["A", "C", "G", "T"]
-        if self.sequence == "" or self.sequence is None:
-            self.sequence = "NULL"
-            return self.sequence
-        for i in self.sequence:
-            if i not in bases:
-                self.sequence = "ERROR!!"
-        return self.sequence
-    def len(self):
-        if self.sequence == "NULL" or self.sequence == "ERROR!!":
-            return 0
-        return len(self.sequence)
-
-    def count_base(self, base):
-        return self.sequence.count(base)
+from Seq1 import *
 
 seq1 = Seq()
-seq2 = Seq("TACTA")
+seq2 = Seq("ACTGA")
 seq3 = Seq("Invalid sequence")
 
 print("Sequence 1:", "(Length:", seq1.len(), ")", seq1,)
--- P01/e7.py
+++ P01/e7.py
@@ -1,47 +1,4 @@
-class Seq:
-
-    def __init__(self, sequence = None):
-        self.sequence = sequence
-        if self.check_base() == "ERROR!!":
-            print("INVALID SEQUENCE!")
-        elif self.sequence == "NULL":
-            print("Null sequence created")
-        else:
-            print("New sequence created!")
-
-    def __str__(self):
-        """Method called when the object is being printed"""
-        # -- We just return the string with the sequence
-        return self.sequence
-
-    def check_base(self):
-        bases = ["A", "C", "G", "T"]
-        if self.sequence == "" or self.sequence is None:
-            self.sequence = "NULL"
-            return self.sequence
-        for i in self.sequence:
-            if i not in bases:
-                self.sequence = "ERROR!!"
-        return self.sequence
-    def len(self):
-        if self.sequence == "NULL" or self.sequence == "ERROR!!":
-            return 0
-        return len(self.sequence)
-
-    def count(self):
-        bases = {"A": 0, "C": 0, "T": 0, "G": 0}
-        for base in self.sequence:
-            if base in bases:
-                bases[base] += 1
-        return bases
-
-    def reverse(self):
-        new_seq = ""
-        if self.sequence == "NULL" or self.sequence == "ERROR!!":
-            return self.sequence
-        for c in range(len(self.sequence)):
-            new_seq = self.sequence[c] + new_seq
-        return new_seq
+from Seq1 import *
 
 seq1 = Seq()
 seq2 = Seq("ACTGA")
