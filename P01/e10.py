c19187fbbe4151c691297e5907c9163c2a552447
nachogonnun <i.gonzalezn.2023@alumnos.urjc.es>
2024-02-21 17:32:09 +0100

    session 2 exercises

--- P01/e10.py
+++ P01/e10.py
@@ -4,11 +4,20 @@ from pathlib import Path
 genes = ["U5", "ADA", "FRAT1", "FXN"]
 bases = ["A", "C", "G", "T"]
 
+
 for gene in genes:
     filename = "../S04/sequences/" + gene + ".txt"
     file_contents = Path(filename).read_text()
     seq = Seq(file_contents.replace("\n", ""))
     seq.read_fasta(file_contents)
-    max_base = max(bases, key=lambda base: seq.seq_count(base))
-    count = seq.seq_count(max_base)
-    print("Gene {} most common nucleotide is {} ({})".format(gene, max_base, count))
+
+    max_count = 0
+    max_base = None
+    for base in bases:
+        count = seq.seq_count(base)
+        if count > max_count:
+            max_base = base
+            max_count = count
+
+    print("Gene {} most common nucleotide is {} ({})".format(gene, max_base, max_count))
+
