6b742850fdd2c10259ce3793120f4924b8f15105
nachogonnun <i.gonzalezn.2023@alumnos.urjc.es>
2024-02-28 15:17:59 +0100

    session 2 exercises

--- S08/server.py
+++ S08/server.py
@@ -2,7 +2,7 @@ import socket
 
 # SERVER IP, PORT
 PORT = 8081
-IP = "212.128.255.64" # depends on the computer the server is running
+IP = "212.128.255.94" # depends on the computer the server is running
 
 while True:
   # -- Ask the user for the message
