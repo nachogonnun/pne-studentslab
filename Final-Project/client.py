990d4570f28add930b7033d021a9e4bf79ed62a8
nachogonnun <i.gonzalezn.2023@alumnos.urjc.es>
2024-05-16 14:48:17 +0200

    .

--- Final-Project/client.py
+++ Final-Project/client.py
@@ -8,24 +8,28 @@ print(f"\nConnecting to server: {SERVER}:{PORT}\n")
 
 conn = http.client.HTTPConnection(SERVER, PORT)
 
-url_s = "/listSpecies?json=1"
-url_s2 = "/listSpecies?limit=1&json=1"
-url_k = "/karyotype?species=Shrew+mouse&json=1"
-url_c = "/chromosomeLength?species=mouse&chromo=18&json=1"
-url_seq = "/geneSeq?gene=FRAT1&json=1"
-url_info = "/geneInfo?gene=FRAT1&json=1"
-url_calc = "/geneCalc?gene=FRAT1&json=1"
-url_list = "/geneList?chromo=9&start=22125500&end=22136000&json=1"
+URLS = [
+    "/listSpecies?json=1",
+    "/listSpecies?limit=10&json=1",
+    "/karyotype?species=Shrew+mouse&json=1",
+    "/chromosomeLength?species=mouse&chromo=18&json=1",
+    "/geneSeq?gene=FRAT1&json=1",
+    "/geneInfo?gene=FRAT1&json=1",
+    "/geneCalc?gene=FRAT1&json=1",
+    "/geneList?chromo=9&start=22125500&end=22136000&json=1"
+]
 
-conn.request("GET", url_list)
+for url in URLS:
 
-response = conn.getresponse()
+    conn.request("GET", url)
 
-print(f"Response received!: {response.status} {response.reason}\n")
+    response = conn.getresponse()
 
-server_information = response.read().decode("utf-8")
+    print(f"Response received!: {response.status} {response.reason}\n")
 
-json_object = json.loads(server_information)
-print("Data received from IGNACIO'S server:")
-print()
-print(json.dumps(json_object, indent=4))
+    server_information = response.read().decode("utf-8")
+
+    json_object = json.loads(server_information)
+    print("Data received from IGNACIO'S server:")
+    print()
+    print(json.dumps(json_object, indent=4))
